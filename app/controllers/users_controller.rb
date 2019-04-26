b09c53e678944dd242c43ac5a8b6e98786e5eabe
Megan Fago <meganlaynefago@gmail.com>
2019-04-26 14:42:18 -0400

    add routes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        rneder json: @user, status: :ok
+    end 
+
+    def create
+        @user = User.create(user_params)
+        if @user
+            render json: @user, status: :ok
+        else
+            render json: {errors: @user.errors.full_messages},
+            status: :unprocessable_entity
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        @user.update(user_params)
+    end 
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end 
+
+    private 
+
+    def user_params 
+        params.permit(:username, :name, :email)
+    end
+
 end
