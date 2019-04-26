95d03f5142c2863ff99a44a12e0e851d6f91eee2
Megan Fago <meganlaynefago@gmail.com>
2019-04-26 14:59:51 -0400

    workout routes up and ready to go

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,38 @@
 class WorkoutsController < ApplicationController
+
+    def index 
+        @workouts = Workout.all 
+        render json: @workouts, status: :ok
+    end 
+
+    def show
+        @workout = Workout.find(params[:id])
+        render json: @workout, status: :ok
+    end 
+
+    def create
+        @workout = Workout.create(workout_params)
+        if @workout
+            render json: @workout, status: :ok
+        else
+            render json: {errors: @workout.errors.full_messages},
+            status: :unprocessable_entity
+    end 
+
+    def update
+        @workout = Workout.find(params[:id])
+        @workout.update(workout_params)
+    end 
+
+    def destroy
+        @workout = Workout.find(params[:id])
+        @workout.destroy
+    end 
+
+    private 
+
+    def workout_params
+        params.permit(:name, :user_id)
+    end 
+
 end
