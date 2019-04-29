81d360c8bb45d4d964c265d17d231a2e0ff9942f
Megan Fago <meganlaynefago@gmail.com>
2019-04-29 11:53:26 -0400

    updated routes and tested all in postman

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :workouts 
     has_many :workout_exercises, through: :workouts
+    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :email, presence: true, length: { minimum: 2 }, uniqueness: true
 end
