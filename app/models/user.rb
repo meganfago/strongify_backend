79c10e2dc928ab24f37fb8f3d7d70a6617ad244a
Megan Fago <meganlaynefago@gmail.com>
2019-04-29 19:30:49 -0400

    attempted auth still have work to do.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :workouts 
     has_many :workout_exercises, through: :workouts
-    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+
+    validates :name, presence: true, length: { minimum: 2 }
     validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
-    validates :email, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :email, presence: true,uniqueness: true, format: { with: /.+\@.+\ ..+/ }
+   
 end
