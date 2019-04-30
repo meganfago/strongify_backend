c416869ec28a83daee9a0acb00217c97fb2303a9
Megan Fago <meganlaynefago@gmail.com>
2019-04-30 16:25:35 -0400

    AUTH SORT OF WORKS!!!!!

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :workouts
   resources :users
 
-  post '/login', to: 'login#login', as: 'login'
+  post '/login', to: 'auth#login', as: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
