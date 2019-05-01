59324520ce83d2d5b9c7f83d000265c6eeb80155
Megan Fago <meganlaynefago@gmail.com>
2019-05-01 15:04:22 -0400

    spacing

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,14 @@
 class AuthController < ApplicationController
-   
-        def login
-            @user = User.find_by(username: params[:username])
-            if @user && @user.authenticate(params[:password])
-                ## then do whatever to log them in
-                ## generate token that contains a user id
-                token = JWT.encode({userId: @user.id}, ENV['SECRET'], 'HS256')
-                render json: {token: token, username: @user.username}, status: :ok 
-            else
-                render json: { error: "Couldn't log in" }, status: :unauthorized
+
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            ## then do whatever to log them in
+            ## generate token that contains a user id
+            token = JWT.encode({userId: @user.id}, ENV['SECRET'], 'HS256')
+            render json: {token: token, username: @user.username}, status: :ok 
+        else
+            render json: { error: "Couldn't log in" }, status: :unauthorized
         end 
     end
 end 
